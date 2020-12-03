d72c58fcbbf218cc7359fdc5928f8b338cde80d2
Josh <josh.handon@gmail.com>
2020-12-03 18:18:45 -0500

    commit tree 8cda990d59b8731f57a0d9601cdf9c1599ea254e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+class Dog
+
+    @@all = []
+    def initialize(name)
+        @name= (name)
+        self.save
+        #@@all << self
+    end
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all.clear
+    end
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+    def save
+        @@all << self
+    end
+    def name
+        @name
+    end
+end
